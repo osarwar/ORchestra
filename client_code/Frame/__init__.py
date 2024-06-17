1cdf026b4478f2bfff5ca9658957a5f94eb188d3
osarwar@mdanderson.edu <osarwar@mdanderson.edu>
2024-06-17 19:17:35 +0000

    Created orchestra logo.png; Edited Frame

--- client_code/Frame/__init__.py
+++ client_code/Frame/__init__.py
@@ -24,7 +24,7 @@ class Frame(FrameTemplate):
     self.sales_page_link.background = app.theme_colors['Primary Container']
     
 
-  def sales_page_link_click(self, **event_args):
+  def case_duration_page_click(self, **event_args):
     """This method is called when the link is clicked"""
     #Clear the content panel and add the Sales Form
     self.content_panel.clear()
@@ -33,7 +33,7 @@ class Frame(FrameTemplate):
     self.sales_page_link.background = app.theme_colors['Primary Container']
     self.reports_page_link.background = "transparent"
 
-  def reports_page_link_click(self, **event_args):
+  def optimizer_page_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     #Clear the content panel and add the Reports Form
     self.content_panel.clear()
